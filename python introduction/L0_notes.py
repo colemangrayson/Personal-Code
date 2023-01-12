be3b5f589148f2bcd9a26c9147c3c3eb811949c8
YGrayson Coleman <graysoncoleman@icloud.com>
2023-01-12 16:31:37 -0500

    (Knight "finished") ("piece" input unfinished)

--- python introduction/L0_notes.py
+++ python introduction/L0_notes.py
@@ -1,5 +1,24 @@
 """Syntax Notes Applicable to Python"""
-#For Loop
-#While Loop
-#If/Else Statement
-#Elif Statement
+
+#Classes
+class Chess:
+    def knight(self, path):
+        legal = ["1", "2", "4", "5", "7", "8", "10", "11"]
+        if path in legal:
+            print("knight to " + user_path)
+        else:
+            print("not a legal move")
+
+#Variables
+white = Chess()
+black = Chess()
+legal = ["1", "2", "4", "5", "7", "8", "10", "11"]
+
+#Code
+while True:
+    piece = str(input("Your Piece: "))
+    piece = Chess()
+    user_path = str(input("Your Move: "))
+    white.piece(user_path)
+    if user_path in legal:
+        break

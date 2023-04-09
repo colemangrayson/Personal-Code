57efa9bc8f91b1f1e835274d20b27714038f7514
YGrayson Coleman <graysoncoleman@icloud.com>
2023-04-09 16:44:00 -0400

    (4/9/23) Opening ChatGPT API

--- python introduction/L4_ChatGPT_ed.py
+++ python introduction/L4_ChatGPT_ed.py
@@ -1,5 +1,29 @@
-"""Practicing different techniques taught by ChatGPT"""
+"""Opening ChatGPT API"""
 
-class Person(self, age, height, ethnicity):
-    def age(self):
-        print("age")
+import requests
+
+# Set up the API endpoint and parameters
+url = 'https://api.openai.com/v1/chat/completions'
+headers = {
+    'Authorization': 'Bearer YOUR_API_KEY',
+    'Content-Type': 'application/json'
+}
+data = {
+    'messages': [
+        {'role': 'system', 'content': 'You: Hello'},
+        {'role': 'user', 'content': 'User: How can you help me?'}
+    ],
+    'temperature': 0.8,
+    'max_tokens': 50
+}
+
+# Make the API request
+response = requests.post(url, headers=headers, json=data)
+
+# Parse the response
+if response.status_code == 200:
+    result = response.json()
+    completions = result['choices'][0]['message']['content']
+    print('ChatGPT Response:', completions)
+else:
+    print('Error:', response.text)
